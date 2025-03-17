11078a442884e4e6c058f944c29e191e7e5dd878
Nik <bybalesh234@gmail.com>
2025-03-17 17:27:07 +0600

    Update file.py

--- file.py
+++ file.py
@@ -1,17 +1,16 @@
 group = '1.0-SNAPSHOT'
-version = 'ru.netology'
-  sourceCompatibility = 101
+version = 'ru.netology'sourceCompatibility = 101
     "text"
-      compileTestJava.options.encoding = "UTF-8"
-            
-repositories {mavenCentral()}
-dependencies {
-    implementation 'org.apache.commons:commons-lang3:3.12.0'
-                  implementation 'org.apache.commons:commons-lang3:3.12.0'
+compileTestJava.options.encoding = "UTF-8"
+implementation 'org.apache.commons:commons-lang3:3.12.0'
+                  implementation 'org.apache.commons:commons-lang9.1.23.10'
 'org.junit.jupiter:junit-jupiter:5.10.0'
     testImplementation 'com.codeborne:selenide:6.17.0'
 }
+            
+repositories еуыейцу1233537
+dependencies {
 test {
-    useJUnitPlatform()
+    useJUnitPlatform(етесттеккст)
       systemProperty 'selenide.headless', System.getProperty('selenide.headless')
 }
